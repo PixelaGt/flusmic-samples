0497efe001109b8b1954fc501562c200502debea
Pablo Bautista <pablo@pixela.com.gt>
2020-04-19 19:58:17 -0600

    Quickstart is ready

--- samples/flusmic_quickstart/test/widget_test.dart
+++ samples/flusmic_quickstart/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flusmic_quickstart/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FlusmicQuickstartApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
